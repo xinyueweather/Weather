8e479c7c52ac9a16efd231fac68aa37d9152417a
孙骄 <xzzzf.dsx@gmail.com>
2019-01-04 14:27:39 +0800

    引入和风天气代码
    目前问题：需要采用SQL

--- app/src/main/java/com/example/admin/xinyueapp/activity/HomePageActivity.java
+++ app/src/main/java/com/example/admin/xinyueapp/activity/HomePageActivity.java
@@ -3,10 +3,19 @@ package com.example.admin.xinyueapp.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.google.gson.Gson;
 
 import com.example.admin.xinyueapp.R;
+
+import java.util.List;
+
+import interfaces.heweather.com.interfacesmodule.bean.Lang;
+import interfaces.heweather.com.interfacesmodule.bean.Unit;
+import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
+import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -23,6 +32,18 @@ public class HomePageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        HeWeather.getWeatherNow(this, "CN101010100", Lang.CHINESE_SIMPLIFIED, Unit.METRIC,
+                new HeWeather.OnResultWeatherNowBeanListener() {
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i("Log", "onError: ", e);
+                    }
+
+                    @Override
+                    public void onSuccess(List<Now> dataObject) {
+                        Log.i("Log", "onSuccess: " + new Gson().toJson(dataObject));
+                    }
+                });
 
     }
 
