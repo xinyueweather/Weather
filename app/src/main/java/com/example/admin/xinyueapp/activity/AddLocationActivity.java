b13f5241815ed6e4622af40a4bcc8bb79deb9b18
孙骄 <xzzzf.dsx@gmail.com>
2019-01-06 14:15:12 +0800

    添加库

--- app/src/main/java/com/example/admin/xinyueapp/activity/AddLocationActivity.java
+++ app/src/main/java/com/example/admin/xinyueapp/activity/AddLocationActivity.java
@@ -1,8 +1,12 @@
 package com.example.admin.xinyueapp.activity;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.text.TextUtils;
 import android.widget.SearchView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 import com.example.admin.xinyueapp.R;
@@ -16,6 +20,4 @@ public class AddLocationActivity extends StartActivity {
         SearchView locationSearch = (SearchView) findViewById(R.id.location_search);
         locationSearch.setIconifiedByDefault(false);
     }
-
-
 }
