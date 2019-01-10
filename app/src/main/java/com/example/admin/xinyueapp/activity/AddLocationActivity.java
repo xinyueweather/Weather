2cd02fd2f10997fe834900affef9178f275cef09
Winterwatertoocold <2467559321@qq.com>
2019-01-10 09:40:06 +0800

    主要天气信息获取

--- app/src/main/java/com/example/admin/xinyueapp/activity/AddLocationActivity.java
+++ app/src/main/java/com/example/admin/xinyueapp/activity/AddLocationActivity.java
@@ -1,31 +1,43 @@
 package com.example.admin.xinyueapp.activity;
 
-import android.Manifest;
 import android.app.Activity;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.SimpleAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.admin.xinyueapp.R;
 import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import java.io.IOException;
+import android.app.Activity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.widget.*;
+import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 import interfaces.heweather.com.interfacesmodule.bean.Lang;
+import interfaces.heweather.com.interfacesmodule.bean.basic.Basic;
 import interfaces.heweather.com.interfacesmodule.bean.search.Search;
+import interfaces.heweather.com.interfacesmodule.bean.weather.now.Now;
+import interfaces.heweather.com.interfacesmodule.view.HeConfig;
 import interfaces.heweather.com.interfacesmodule.view.HeWeather;
 
 public class AddLocationActivity extends Activity implements SearchView.OnQueryTextListener {
@@ -34,25 +46,19 @@ public class AddLocationActivity extends Activity implements SearchView.OnQueryT
     private SearchView locationSearch;
     private ListView locations;
     //自动完成的列表
-    ArrayList<String> searchResults = new ArrayList<>();
-    ArrayList<String> admins = new ArrayList<>();
-    ArrayList<String> cids = new ArrayList<>();
-    ArrayList<HashMap<String, String>> itemsShown = new ArrayList<HashMap<String, String>>();
+    private final ArrayList<String> searchResults = new ArrayList<>();
+    private final ArrayList<String> admins = new ArrayList<>();
+    private final ArrayList<String> cids = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
         locations=(ListView)findViewById(R.id.locations);
-        freshList();
+        locations.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,searchResults));
+        locations.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,admins));
         //locations.setTextFilterEnabled(true);
-        //点击列表项时激发该方法
-        locations.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(AddLocationActivity.this, "您选择了第" + id + "个项目, 该地点是：" + itemsShown.get(position) + ", 它的 cid 是: "+ cids.get(position), Toast.LENGTH_LONG).show();
-            }
-        });
 
         locationSearch=(SearchView)findViewById(R.id.location_search);
         //设置该SearchView默认是否自动缩小为图标
@@ -61,44 +67,8 @@ public class AddLocationActivity extends Activity implements SearchView.OnQueryT
         locationSearch.setOnQueryTextListener(this);
         //设置该SearchView显示搜索按钮
         locationSearch.setSubmitButtonEnabled(true);
-    }
 
-    public boolean clearList(){
-        //清空所有ArrayList
-        if (searchResults.size()>0){
-            for(int i = searchResults.size() ; i > 0 ; i-- ){
-                searchResults.remove(0);
-            }
-        }
-        if (admins.size()>0){
-            for(int i = admins.size() ; i > 0 ; i-- ){
-                admins.remove(0);
-            }
-        }
-        if (cids.size()>0){
-            for(int i = cids.size() ; i > 0 ; i-- ){
-                cids.remove(0);
-            }
-        }
-        if (itemsShown.size()>0){
-            for(int i = itemsShown.size() ; i > 0 ; i-- ){
-                itemsShown.remove(0);
-            }
-        }
-        return true;
-    }
 
-    public boolean freshList(){
-        locations.setAdapter(new SimpleAdapter(
-                this,
-                itemsShown,//数据来源
-                R.layout.item_add_location,//ListItem的XML实现
-                new String[] {"locationText", "adminText"},//与ListItem对应的子项
-                new int[] {R.id.locationText,R.id.adminText//item_add_location的XML文件里面的两个TextView的id
-                }));
-        //locations.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemsShown));
-        //locations.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,admins));
-        return true;
     }
 
     @Override
@@ -111,43 +81,66 @@ public class AddLocationActivity extends Activity implements SearchView.OnQueryT
     @Override
     public boolean onQueryTextChange(String newText) {
         // TODO Auto-generated method stub
-        clearList();
-        freshList();
-        if(TextUtils.isEmpty(newText)) {} else {
-            //使用用户输入的内容查询并输出结果
-            HeWeather.getSearch(this, newText, "world", 20, Lang.CHINESE_SIMPLIFIED, new HeWeather.OnResultSearchBeansListener() {
-                //基于用户输入，通过和风API获取查询结果
+        if(TextUtils.isEmpty(newText))
+        {
+            //清除ListView
+            if (searchResults.size()>0){
+                for(int i = 0 ; i < searchResults.size() ; i++ ){
+                    searchResults.remove(i);
+                }
+            }
+            if (admins.size()>0){
+                for(int i = 0 ; i < admins.size() ; i++ ){
+                    admins.remove(i);
+                }
+            }
+            if (cids.size()>0){
+                for(int i = 0 ; i < cids.size() ; i++ ){
+                    cids.remove(i);
+                }
+            }
+        }
+        else
+        {
+            //使用用户输入的内容对ListView的列表项进行过滤
+            HeWeather.getSearch(this, newText, "world", 10, Lang.CHINESE_SIMPLIFIED, new HeWeather.OnResultSearchBeansListener() {
                 @Override
                 public void onError(Throwable throwable) {
                     Log.i("Log", "onError: ", throwable);
                 }
+
                 @Override
                 public void onSuccess(Search search) {
+                    Log.i("Log", "onSuccess: " + new Gson().toJson(search));
                     Gson gson = new Gson();
-                    String jsondata = gson.toJson(search);          //把dataObject转换成json字符串，存储在jsondata中。
-                    try {
-                        JSONObject jsonObject = new JSONObject(jsondata); //将字符串格式的jsondata转为jsonobject
-                        JSONArray basic = jsonObject.getJSONArray("basic");//获取basic字段，也就是真正有用的数据部分
-                        for (int i=0; i < basic.length(); i++){
-                            JSONObject nowBases = basic.getJSONObject(i);//获取第 i 个JSON object
+                    String jsondata = gson.toJson(search);          //把dataObject转换成json字符串
+                    Log.i("Log", "onSuccess0: " + jsondata);
+                    try
+                    {
+                        //Log.i("Log", "onSuccess1: try.");
+                        JSONArray jsonArray = new JSONArray(jsondata);
+                        //Log.i("Log", "onSuccess1: try,try.");
+                        JSONObject jsonObject = jsonArray.getJSONObject(0);
+                        //Log.i("Log", "onSuccess1: try,try,try." + jsonObject);
+                        JSONObject nowBases = jsonObject.getJSONObject("basic");
+                        for (int i=0; i < nowBases.length(); i++){
+                            //Log.i("Log", "onSuccess1: " + i);
                             searchResults.add(nowBases.getString("location"));
-                            admins.add(nowBases.getString("admin_area") + " , " + nowBases.getString("parent_city"));
+                            String admin = nowBases.getString("admin_area") + " , " + nowBases.getString("parent_city");
+                            admins.add(admin);
                             cids.add(nowBases.getString("cid"));
-                            HashMap<String, String> item = new HashMap<String, String>();
-                            item.put("locationText",searchResults.get(i));
-                            item.put("adminText",admins.get(i));
-                            itemsShown.add(item);
-                            //Log.i("Log","onSuccess: " + searchResults.get(i) + " , " + admins.get(i) + " , " + cids.get(i));
+                            //Log.i("Log","onSuccess2: " + searchResults.get(i) + " , " + admins.get(i) + " , " + cids.get(i));
                         }
                     }
-                    catch (Exception e) {
+                    catch (Exception e)
+                    {
                         e.printStackTrace();
                     }
-                    freshList();
                 }
 
             });
-            //locations.setFilterText(newText);
+            locations.setFilterText(newText);
+
         }
         return true;
     }
@@ -156,47 +149,5 @@ public class AddLocationActivity extends Activity implements SearchView.OnQueryT
     public boolean onQueryTextSubmit(String query) {
         Toast.makeText(getApplicationContext(),this.getString(R.string.please_click_results),Toast.LENGTH_SHORT).show();
         return true;
-    }
-
-    //点击搜索栏右侧的位置图标时激发该方法
-    public void searchWithGPS(View view){
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                0);
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                0);
-        clearList();
-        freshList();
-        HeWeather.getSearch(this, 10, new HeWeather.OnResultSearchBeansListener() {
-            //基于用户输入，通过和风API获取查询结果
-            @Override
-            public void onError(Throwable throwable) {
-                Log.i("Log", "onError: ", throwable);
-            }
-            @Override
-            public void onSuccess(Search search) {
-                Gson gson = new Gson();
-                String jsondata = gson.toJson(search);          //把dataObject转换成json字符串，存储在jsondata中。
-                try {
-                    JSONObject jsonObject = new JSONObject(jsondata); //将字符串格式的jsondata转为jsonobject
-                    JSONArray basic = jsonObject.getJSONArray("basic");//获取basic字段，也就是真正有用的数据部分
-                    JSONObject nowBases = basic.getJSONObject(0);//获取第 i 个JSON object
-                    searchResults.add(nowBases.getString("location"));
-                    admins.add(nowBases.getString("admin_area") + " , " + nowBases.getString("parent_city"));
-                    cids.add(nowBases.getString("cid"));
-                    HashMap<String, String> item = new HashMap<String, String>();
-                    item.put("locationText",searchResults.get(0));
-                    item.put("adminText", admins.get(0));
-                    itemsShown.add(item);
-                    //rLog.i("Log","onSuccess: " + searchResults.get(0) + " , " + admins.get(0) + " , " + cids.get(0));
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-                freshList();
-            }
-
-        });
     }
 }
